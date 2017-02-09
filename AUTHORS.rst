ac6cbeac543f40c0da1f4e506da6f74783244e10
Denis Bobrov <delneg@Yandex.ru>
2017-02-10 00:22:20 +0300

    Added models with ugettext translate Added dialogs urls Added oleg to dev lead in AUTHORS.rst Changed setup.py to be more relevant

--- AUTHORS.rst
+++ AUTHORS.rst
@@ -6,6 +6,7 @@ Development Lead
 ----------------
 
 * delneg <tech@bearle.ru>
+* guitarmustafa <oleg5432101@rambler.ru>
 
 Contributors
 ------------
--- django_private_chat/models.py
+++ django_private_chat/models.py
@@ -1,16 +1,28 @@
 # -*- coding: utf-8 -*-
 
 from django.db import models
+from model_utils.models import TimeStampedModel, SoftDeletableModel
+from django.conf import settings
+from django.template.defaultfilters import date as dj_date
+from django.utils.translation import ugettext as _
 
-from model_utils.models import TimeStampedModel
-
-
-class Message(TimeStampedModel):
-    pass
-    
 
 class Dialog(TimeStampedModel):
-    pass
-    
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name=_("Dialog owner"), related_name="selfDialogs")
+    opponent = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name=_("Dialog opponent"))
+
+    def __str__(self):
+        return _("Chat with ") + self.opponent.username
 
 
+class Message(TimeStampedModel, SoftDeletableModel):
+    dialog = models.ForeignKey(Dialog, verbose_name=_("Dialog"), related_name="messages")
+    sender = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name=_("Author"), related_name="messages")
+    text = models.TextField(verbose_name=_("Message text"))
+    all_objects = models.Manager()
+
+    def get_formatted_create_datetime(self):
+        return dj_date(self.created, settings.DATETIME_FORMAT)
+
+    def __str__(self):
+        return self.sender.username + "(" + self.get_formatted_create_datetime() + ") - '" + self.text + "'"
