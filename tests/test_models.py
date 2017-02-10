2932bd8d9e32b15dd542a1f1c3bd57dfb60ab336
Oleg <oleg5432101@rambler.ru>
2017-02-10 16:41:16 +0300

    added models tests

--- tests/test_models.py
+++ tests/test_models.py
@@ -8,18 +8,38 @@ test_django-private-chat
 Tests for `django-private-chat` models module.
 """
 
-from django.test import TestCase
+from test_plus.test import TestCase
+from django_private_chat.models import *
 
-from django_private_chat import models
 
-
-class TestDjango_private_chat(TestCase):
-
+class DialogMethodTest(TestCase):
     def setUp(self):
-        pass
+        self.dialog = Dialog()
+        self.dialog.owner = self.make_user(username="owuser")
+        self.dialog.opponent = self.make_user(username="opuser")
 
-    def test_something(self):
-        pass
+    def test_str_method(self):
+        self.assertEqual(str(self.dialog), "Chat with opuser")
 
-    def tearDown(self):
-        pass
+
+class MessageMethodTest(TestCase):
+    def setUp(self):
+        self.dialog = Dialog()
+        self.dialog.owner = self.make_user(username="owuser")
+        self.dialog.opponent = self.make_user(username="opuser")
+        self.dialog.save()
+        self.message = Message()
+        self.message.dialog = self.dialog
+        self.message.sender = self.dialog.owner
+        self.message.text = "text about something interesting"
+        self.message.save()
+
+    def test_str_method(self):
+        self.assertEqual(str(self.message), "owuser(" +
+                         self.message.modified.strftime('%x %X') + ") - 'text about something interesting'")
+
+    def test_soft_delete(self):
+        msg = self.message
+        self.message.delete()
+        self.assertNotIn(msg, Message.objects.all())
+        self.assertIn(msg, Message.all_objects.filter(is_removed=True))
