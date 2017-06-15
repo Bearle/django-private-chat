b05d60cde539515e248863611c3d4153be29dc77
grantal <AlexV348@gmail.com>
2017-06-15 15:46:59 -0700

    fixed the remaining tests

--- tests/test_models.py
+++ tests/test_models.py
@@ -13,6 +13,7 @@ from django_private_chat.models import *
 
 
 class DialogMethodTest(TestCase):
+
     def setUp(self):
         self.dialog = Dialog()
         self.dialog.owner = self.make_user(username="owuser")
@@ -23,6 +24,7 @@ class DialogMethodTest(TestCase):
 
 
 class MessageMethodTest(TestCase):
+
     def setUp(self):
         self.dialog = Dialog()
         self.dialog.owner = self.make_user(username="owuser")
@@ -35,8 +37,23 @@ class MessageMethodTest(TestCase):
         self.message.save()
 
     def test_str_method(self):
-        self.assertEqual(str(self.message), "owuser(" +
-                         self.message.modified.strftime('%x %X') + ") - 'text about something interesting'")
+        """
+        Makes sure message text and something relating to the date
+        is in the string function output
+        """
+        mes = str(self.message)
+        text = self.message.text
+        hour = self.message.modified.strftime('%H')
+        tfhour = self.message.modified.strftime('%I')  # 24-hour clock
+        min = self.message.modified.strftime('%M')
+        day = self.message.modified.strftime('%d')
+        month = self.message.modified.strftime('%m')
+        lmon = self.message.modified.strftime('%b')  # month abbreviation
+        self.assertTrue((text in mes) and
+                        (hour in mes or tfhour in mes) and
+                        (min in mes) and
+                        (day in mes) and
+                        (month in mes or lmon in mes))
 
     def test_soft_delete(self):
         msg = self.message
