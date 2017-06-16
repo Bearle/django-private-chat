9c42c7c4359d5d96febeef29ffb516e574386a39
Alex Grant <AlexV348@gmail.com>
2017-06-16 10:02:45 -0700

    Changed single quotes to double quotes

--- tests/settings.py
+++ tests/settings.py
@@ -22,7 +22,7 @@ INSTALLED_APPS = [
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sites",
-    'django.contrib.sessions',
+    "django.contrib.sessions",
     "django_private_chat",
 ]
 
