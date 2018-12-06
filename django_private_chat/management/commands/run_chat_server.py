dcba8623ac1ecfbb0bbefa514b26375ced0b96ea
Emeka Icha <emeka.icha@gmail.com>
2018-12-06 17:18:32 +0300

    (fix) python3.7 compatibility with async

--- django_private_chat/management/commands/run_chat_server.py
+++ django_private_chat/management/commands/run_chat_server.py
@@ -29,7 +29,12 @@ class Command(BaseCommand):
         else:
             ssl_context = None
 
-        asyncio.async(
+        if hasattr(asyncio, "ensure_future"):
+            ensure_future = asyncio.ensure_future 
+        else:
+            ensure_future = getattr(asyncio, "async")
+
+        ensure_future(
             websockets.serve(
                 handlers.main_handler,
                 settings.CHAT_WS_SERVER_HOST,
@@ -39,12 +44,12 @@ class Command(BaseCommand):
         )
 
         logger.info('Chat server started')
-        asyncio.async(handlers.new_messages_handler(channels.new_messages))
-        asyncio.async(handlers.users_changed_handler(channels.users_changed))
-        asyncio.async(handlers.gone_online(channels.online))
-        asyncio.async(handlers.check_online(channels.check_online))
-        asyncio.async(handlers.gone_offline(channels.offline))
-        asyncio.async(handlers.is_typing_handler(channels.is_typing))
-        asyncio.async(handlers.read_message_handler(channels.read_unread))
+        ensure_future(handlers.new_messages_handler(channels.new_messages))
+        ensure_future(handlers.users_changed_handler(channels.users_changed))
+        ensure_future(handlers.gone_online(channels.online))
+        ensure_future(handlers.check_online(channels.check_online))
+        ensure_future(handlers.gone_offline(channels.offline))
+        ensure_future(handlers.is_typing_handler(channels.is_typing))
+        ensure_future(handlers.read_message_handler(channels.read_unread))
         loop = asyncio.get_event_loop()
         loop.run_forever()
