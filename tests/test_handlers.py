a0b14b7f2b0045d7516e8e081de836d6771db277
Emeka Icha <emeka.icha@gmail.com>
2019-01-10 17:26:55 +0300

    setup module for testing handlers

---
+++ tests/test_handlers.py
@@ -0,0 +1,12 @@
+import asyncio
+import json
+
+from django.contrib.auth import get_user_model
+import websockets
+
+from django_private_chat.models import *
+
+# TODO
+# create a client
+# create a server
+# test they can communicate
