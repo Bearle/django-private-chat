f50d2a53f89dc75c03f89ce4365b52147bd46e38
Emeka Icha <emeka.icha@gmail.com>
2019-01-21 12:04:25 +0300

    add test for users_changed and new_messages handlers

--- tests/test_handlers.py
+++ tests/test_handlers.py
@@ -23,6 +23,36 @@ def mock_is_typing_handler_coro(stream):
     res = yield from stream.get()
     return res
 
+@asyncio.coroutine
+def mock_read_message_handler_coro(stream):
+    res = yield from stream.get()
+    return res 
+
+@asyncio.coroutine
+def mock_new_messages_handler_coro(stream):
+    res = yield from stream.get()
+    return res 
+
+@asyncio.coroutine
+def mock_gone_offline_handler_coro(stream):
+    res = yield from stream.get()
+    return res
+
+@asyncio.coroutine
+def mock_gone_online_handler_coro(stream):
+    res = yield from stream.get()
+    return res
+
+@asyncio.coroutine
+def mock_check_online_handler_coro(stream):
+    res = yield from stream.get()
+    return res 
+
+@asyncio.coroutine
+def mock_users_changed_handler_coro(stream):
+    res = yield from stream.get()
+    return res 
+
 @pytest.mark.asyncio
 @asyncio.coroutine
 def test_is_typing_handler():
@@ -37,5 +67,42 @@ def test_is_typing_handler():
     mocked.is_typing_handler = mock_is_typing_handler_coro
 
     result = yield from mocked.is_typing_handler(channels.is_typing)
-    print('result', str(result))
     assert result == data
+    # assert mocked is called once with is_typing channels
+
+@pytest.mark.asyncio
+@asyncio.coroutine
+def test_new_message_handler():
+    yield from channels.new_messages.put(
+        {
+            'type': 'new_message'
+        }
+    )
+
+    assert channels.new_messages.empty() == False 
+
+    mocked = Mock(handlers)
+    mocked.new_messages_handler = mock_new_messages_handler_coro
+
+    result = yield from mocked.new_messages_handler(channels.new_messages)
+
+    assert result is not None 
+
+
+@pytest.mark.asyncio
+@asyncio.coroutine
+def test_users_changed_handler():
+    yield from channels.users_changed.put(
+        {
+            'type': 'users_changed'
+        }
+    )
+
+    assert channels.users_changed.empty() == False 
+
+    mocked = Mock(handlers)
+    mocked.users_changed_handler = mock_users_changed_handler_coro
+
+    result = yield from mocked.users_changed_handler(channels.users_changed)
+
+    assert result is not None 
