2610b3414bcb03bf5cc23a9933f9208a6297bd30
Emeka Icha <emeka.icha@gmail.com>
2019-01-22 12:27:51 +0300

    refactor is_typing_handler test function

--- tests/test_handlers.py
+++ tests/test_handlers.py
@@ -1,28 +1,12 @@
 import asyncio
-import json
+# import json
 from unittest.mock import Mock
 import pytest
-from django.contrib.auth import get_user_model
-import websockets
+# from django.contrib.auth import get_user_model
+# import websockets
 
 from django_private_chat import handlers
 from django_private_chat import channels
-
-
-def get_typing_data():
-    data = {
-        'type': 'is_typing',
-        'session_key': 1,
-        'username': 'user',
-        'typing': True
-    }
-    return data
-
-
-@asyncio.coroutine
-def mock_is_typing_handler_coro(stream):
-    res = yield from stream.get()
-    return res
 
 
 @asyncio.coroutine
@@ -64,19 +48,27 @@ def mock_users_changed_handler_coro(stream):
 @pytest.mark.asyncio
 @asyncio.coroutine
 def test_is_typing_handler():
-    data = get_typing_data()
+    data = {
+        'type': 'is_typing',
+        'session_key': 1,
+        'username': 'user',
+        'typing': True
+    }
 
-    # put data in queue
     yield from channels.is_typing.put(data)
-    assert channels.is_typing.empty() is False 
+    assert channels.is_typing.empty() is False
 
     # mock is_typing_handler
+    @asyncio.coroutine
+    def mock_is_typing_handler_coro(stream):
+        res = yield from stream.get()
+        return res
+
     mocked = Mock(handlers)
     mocked.is_typing_handler = mock_is_typing_handler_coro
 
     result = yield from mocked.is_typing_handler(channels.is_typing)
     assert result == data
-    # assert mocked is called once with is_typing channels
 
 
 @pytest.mark.asyncio
@@ -88,7 +80,7 @@ def test_new_message_handler():
         }
     )
 
-    assert channels.new_messages.empty() == False 
+    assert channels.new_messages.empty() is False
 
     mocked = Mock(handlers)
     mocked.new_messages_handler = mock_new_messages_handler_coro
